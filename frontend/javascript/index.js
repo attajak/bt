b2089be9437b015db3f8d6ad57eb79a075228bf7
Attajak Janrak <janrak@gmail.com>
2026-02-22 05:29:03 +0700

    Initial commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,8 @@
+import "$styles/index.scss"
+import "$styles/syntax-highlighting.css"
+import "bridgetown-quick-search"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "$components/**/*.{js,jsx,js.rb,css}"
+
+console.info("Bridgetown is loaded!")
